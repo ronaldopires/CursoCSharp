8904891171814b2570e75bf33a0a99af02814c07
Ronaldo Carvalho <ronaldo.carvalho@hotmail.com>
2022-05-11 23:36:55 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Comentarios
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Código claro é sempre melhor!");
+        }
+    }
+}
